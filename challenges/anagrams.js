e6d99b0f724e43b6b72174b4bd2ecfdf051dbb0c
La Fountain <github email address>
2019-08-13 11:28:19 -0400

    Upated to file

---
+++ challenges/anagrams.js
@@ -0,0 +1,16 @@
+// 1. Given an array of words, print all anagrams together. For example, if the given array is {“cat”, “dog”, “tac”, “god”, “act”}, then output may be “cat tac act dog god”.
+
+function anagram(array) {}
+
+module.exports = anagram;
+
+/* 
+1.  input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+2. output =  ["ate","eat","tea"], ["nat","tan"], ["bat"]
+*/
+
+// 2. Given an array of strings, return all groups of strings that are anagrams.
+/* 
+ input =  ["STORING", "CARING", "NITROS", "BABY",	"GROTS", 	"TRIGONS", "AGE", "STRONG", "LAWYER", "INTROS ", "WALL", "TROGS"]
+ output =  ["STORING", "NITROS", 	"GROTS", 	"TRIGONS",  "INTROS ",  "TROGS"]
+*/
