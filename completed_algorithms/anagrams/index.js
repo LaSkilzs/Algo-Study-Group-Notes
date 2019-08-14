c48971f77ee0cb4ccd9da64bdf4d2ca6428199d8
Derien Stephens <haniaroadlifestyle@gmail.com>
2019-08-13 23:50:40 -0400

    Derien's Anagram

---
+++ completed_algorithms/anagrams/index.js
@@ -0,0 +1,62 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+
+  let count = 0;
+  let secondCount = 0;
+  let anagram = false;
+
+  for(var i = 0; i < stringA.length - 1; i++){
+
+    count++;
+    secondCount = stringB.length - 1;
+
+    if(stringB.includes(stringA[i]) && count == secondCount){
+      anagram = true;
+    }
+
+    else{
+      anagram = false;
+    }
+  }
+  console.log(anagram);
+}
+
+anagrams("hi", "ih");
+
+module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {};
+//
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//
+//   return charMap;
+// }
